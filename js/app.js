20309ee39a387c0c692a2c866c5f074a356b3ee2
SebastianIsmaelG <gutierrezs.contacto@gmail.com>
2024-07-03 23:41:30 -0400

    Agrega las funciones de encriptar texto

--- js/app.js
+++ js/app.js
@@ -8,3 +8,39 @@ textarea.addEventListener('focus',()=>{
     area_real_oculta.style.display = 'block';
     area_de_bienvenida.style.display = 'none';
 });
+textarea.addEventListener('blur', () => {
+    area_de_bienvenida.style.display = 'none';
+});
+
+function remplazar(elemento){
+    let x = elemento.value;
+    x = x.split(/[^A-Za-z0-9\#\& ]+/g);
+    x = x.join("");
+    elemento.value = x;
+}
+
+function tomar_texto(){
+    var texto = document.getElementById("caja_desencriptada").value;
+    
+    //quitar caracteres especiales
+    minusculas = texto.toLowerCase();
+
+    texto_separado = minusculas.split("");
+
+    texto_separado = texto_separado.map(caracter => {
+        // Convierte caracter de a a x y devuelve para hacer 
+        return caracter === 'e' ? 'enter':
+               caracter === 'i' ? 'imes':
+               caracter === 'a' ? 'ai':
+               caracter === 'o' ? 'ober':
+               caracter === 'u' ? 'ufat':
+               caracter;
+        
+      });
+
+    //join y junta el texto
+    var texto_encriptado = texto_separado.join("");
+    console.log(texto_encriptado);
+    
+      
+}
